7933753481adf0469c47ee1db50336cb2ded5664
Yunkai Zhu <yunkaiz2@illinois.edu>
2018-05-01 23:31:18 -0500

    wind app

--- app/src/main/java/edu/illinois/cs/cs125/afinal/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/afinal/MainActivity.java
@@ -61,6 +61,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {
                 Log.d(TAG, "Direction clicked");
+                callWindDegree();
+            }
+        });
+        final Button gust = findViewById(R.id.gust);
+        gust.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Gust clicked");
+                callWindGust();
             }
         });
         final Button update = findViewById(R.id.update);
@@ -140,4 +149,72 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    void callWindDegree() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "http://api.openweathermap.org/data/2.5/weather?zip=61820,us&appid=" + BuildConfig.API_KEY,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            try {
+                                Log.d(TAG, response.toString(2));
+                                TextView textView = findViewById(R.id.textView);
+                                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                                JsonParser jsonParser = new JsonParser();
+                                JsonElement jsonElement = jsonParser.parse(response.toString());
+                                JsonElement wind = jsonElement.getAsJsonObject().get("wind");
+                                JsonElement deg = wind.getAsJsonObject().get("deg");
+                                String prettyJsonString = gson.toJson(deg);
+                                textView.setText(prettyJsonString);
+                                textView.setVisibility(View.VISIBLE);
+                            } catch (JSONException ignored) {
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    void callWindGust() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "http://api.openweathermap.org/data/2.5/weather?zip=61820,us&appid=" + BuildConfig.API_KEY,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            try {
+                                Log.d(TAG, response.toString(2));
+                                TextView textView = findViewById(R.id.textView);
+                                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                                JsonParser jsonParser = new JsonParser();
+                                JsonElement jsonElement = jsonParser.parse(response.toString());
+                                JsonElement wind = jsonElement.getAsJsonObject().get("wind");
+                                JsonElement gust = wind.getAsJsonObject().get("gust");
+                                String prettyJsonString = gson.toJson(gust);
+                                textView.setText(prettyJsonString);
+                                textView.setVisibility(View.VISIBLE);
+                            } catch (JSONException ignored) {
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
